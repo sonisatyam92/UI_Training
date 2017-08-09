0010d9ddf09591b6c46b3d4a87935c3f67d0297f
sonisatyam92 <sonisatyam92@gmail.com>
2017-08-09 08:17:37 +0530

    Lecture 11

--- lectures/lecture10/main.js
+++ lectures/lecture10/main.js
@@ -4,3 +4,28 @@ for(var i=1;i<=100;i++){
     document.write(i+'<br/>');
 }
 document.write("<br/> ------------------------------------- <br/> SUM : "+ sum);
+
+MyClass('Satyam').getName();
+
+function MyClass(name){
+    return {
+        name : name,
+        getName :function(){
+            return name;
+        }
+
+    }
+
+}
+
+// DOM
+
+
+
+// over laoding
+function myFunct(asda, asdasd){
+
+}
+function myFunct(asda){
+
+}
