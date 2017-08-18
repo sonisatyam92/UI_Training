86375d947f9101a2ec36c04a30fa121d5fd76146
sonisatyam92 <sonisatyam92@gmail.com>
2017-08-18 08:15:21 +0530

    Lecture 14

--- lectures/lecture15/main.js
+++ lectures/lecture15/main.js
@@ -20,14 +20,49 @@
 })();
 
 
-$('#loginBtn').on('click',function(){
+/*$('#loginBtn').on('click',function(){
     //$('.login-box').hide();
     //$('.login-box').fadeOut();
     //$('.login-box').fadeOut('slow');
     //$('.login-box').fadeOut('fast');
     $('.login-box').fadeToggle(3000);
-});
+});*/
 
 $('.head-line').on('click',function(){
     $('.login-box').fadeToggle(3000);
+});
+
+
+
+$(".create-link").on('click',function(){
+   $('.create-link').animate({
+       height:'toggle'
+   });
+});
+
+
+$('#loginBtn').on('click',function(){
+    var user = $('#username').val();
+    if(!user || user.length ===0){
+
+        $('.login-box').animate({
+            left : '-=30px'
+        },100, function(){
+            $('.login-box').animate({
+                left : '+=60px'
+            },100, function(){
+                $('.login-box').animate({
+                    left : '-=60px'
+                },100, function(){
+                    $('.login-box').animate({
+                        left : '+=30px'
+                    },100, function(){
+
+                    });
+                });
+
+            });
+        });
+
+    }
 });
