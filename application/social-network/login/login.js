3080cd7b292d6d79e12f7fdb2fd4f7ef26bec067
sonisatyam92 <sonisatyam92@gmail.com>
2017-09-20 08:51:05 +0530

    Lecture 27

--- application/social-network/login/login.js
+++ application/social-network/login/login.js
@@ -5,7 +5,29 @@
         .controller('LoginController', LoginController);
 
     function LoginController($scope, $http){
+        var vm = $scope;
 
+        vm.username ="";
+        vm.password="";
+        vm.errorMsg="";
+        vm.hasError=false;
+
+        vm.onLogin = function(){
+            if(vm.username && vm.password){
+
+            }else {
+               vm.showErrorMsg("Username and Password fields are mandatory.");
+            }
+        };
+
+        vm.showErrorMsg = function(msg){
+            vm.hasError = true;
+            vm.errorMsg = msg;
+        };
+        vm.clearErrorMsg = function(){
+            vm.hasError = false;
+            vm.errorMsg = null;
+        }
 
     }
 })();
